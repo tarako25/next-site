41e3c59accb1178c3e06ded3a2b4a03f2d0596c1
nishimura <nishimura1603t@gmail.com>
2024-04-05 12:59:47 +0900

    取得データ表示中

--- src/app/components/Articles.tsx
+++ src/app/components/Articles.tsx
@@ -8,16 +8,19 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from '@/components/ui/pagination'
-import { useEffect } from 'react'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
 
 const Articles = () => {
+  const [articles, setArticles] = useState([])
   useEffect(() => {
     getZennArticles()
   }, [])
   const getZennArticles = async () => {
     const res = await fetch('api/getZennArticle')
     const data = await res.json()
-    console.log(data)
+    setArticles(data.articles)
+    console.log(data.articles)
   }
   return (
     <>
@@ -27,41 +30,20 @@ const Articles = () => {
           <h1 className="w-full my-3 p-3 font-bold text-xl">記事一覧</h1>
         </div>
         <div className="flex w-full justify-between flex-wrap">
-          <div className="flex justify-center items-center md:w-[48%] w-[100%] h-[100px] mb-5 px-1 bg-white border border-slate-200 rounded-md">
-            <div className="min-w-[90px] h-[90px] bg-slate-300 rounded"></div>
-            <div className="h-[90px] px-2 py-1">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Possimus, cumque.
-            </div>
-          </div>
-          <div className="flex justify-center items-center md:w-[48%] w-[100%] h-[100px] mb-5 px-1 bg-white border border-slate-200 rounded-md">
-            <div className="min-w-[90px] h-[90px] bg-slate-300 rounded"></div>
-            <div className="h-[90px] px-2 py-1">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Possimus, cumque.
-            </div>
-          </div>
-          <div className="flex justify-center items-center md:w-[48%] w-[100%] h-[100px] mb-5 px-1 bg-white border border-slate-200 rounded-md">
-            <div className="min-w-[90px] h-[90px] bg-slate-300 rounded"></div>
-            <div className="h-[90px] px-2 py-1">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Possimus, cumque.
-            </div>
-          </div>
-          <div className="flex justify-center items-center md:w-[48%] w-[100%] h-[100px] mb-5 px-1 bg-white border border-slate-200 rounded-md">
-            <div className="min-w-[90px] h-[90px] bg-slate-300 rounded"></div>
-            <div className="h-[90px] px-2 py-1">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Possimus, cumque.
-            </div>
-          </div>
-          <div className="flex justify-center items-center md:w-[48%] w-[100%] h-[100px] mb-5 px-1 bg-white border border-slate-200 rounded-md">
-            <div className="min-w-[90px] h-[90px] bg-slate-300 rounded"></div>
-            <div className="h-[90px] px-2 py-1">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Possimus, cumque.
-            </div>
-          </div>
+          {articles.map((article) => (
+            <Link
+              href={`https://zenn.dev/${article.path}`}
+              key={article.id}
+              className="flex justify-start items-center md:w-[48%] w-[100%] h-[100px] mb-5 px-1 bg-white border border-slate-200 rounded-md"
+            >
+              <div className="min-w-[90px] h-[90px] bg-slate-100 rounded flex justify-center items-center">
+                <div className="text-5xl">{article.emoji}</div>
+              </div>
+              <div className="h-[90px] px-2 py-1 font-bold">
+                {article.title}
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
       <Pagination className="mb-5">
